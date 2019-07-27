ae64596ff64e81b19b9a6c7a164b5f1de6d8baf0
Quibnar <dirk.stockton@gmail.com>
2019-07-26 17:03:39 -0700

    TD engine progress

--- protypes/proto_1/js/logic.js
+++ protypes/proto_1/js/logic.js
@@ -25,39 +25,40 @@ function buildTowers(num) {
         myTowers[i] = new tower()
         myTowers[i].x = Math.random() * w
         myTowers[i].y = Math.random() * h
+        console.log(myTowers[i].x)
         myTowers[i].render()
 
     }
 }
 
 function tower() {
+
     this.type = 'wall',
         this.health = 10,
         this.x = 0,
         this.y = 0,
         this.width = 16,
         this.height = 16,
-        this.color = '#883344',
-        this.ctx = document.getElementById('game_canvas').getContext("2d");
-    this.upgrade = function () {
-        if (this.type === 'wall') {
-            this.type = 'tower'
-            this.color = '#ffaa88'
+        this.color = 'rgba( ' + Math.random() * 256 + ', ' + Math.random() * 256 + ', ' + Math.random() * 256 + ', 1.0 )',
+        this.ctx = document.getElementById('game_canvas').getContext("2d"),
+        this.upgrade = function () {
+            if (this.type === 'wall') {
+                this.type = 'tower'
+                this.color = '#ffaa88'
+            }
+        },
+        this.render = function () {
+
+            this.ctx.canvas.width = w;
+            this.ctx.canvas.height = h;
+            //this.ctx.fillStyle = this.color;
+            this.ctx.fillRect(this.x, this.y, 16, 16);
+
         }
-    }
-    this.render = function () {
 
-        var ctx = canvas.getContext("2d");
-        ctx.canvas.width = w;
-        ctx.canvas.height = h;
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.x + 16, this.y + 16);
-
-    }
 }
 
 game.buildLvl()
-
 
 var globalID
 var count = 0
@@ -66,12 +67,14 @@ function repeatOften() {
 
     count++
 
-    //oscillateColor('body', 0.1, 0.8)
+    //oscillateColor('#hud', 0.1, 0.8)
 
     function oscillateColor(element, frequency, amplitude) {
+
         formula = Math.sin(count / 6 * frequency) * amplitude
         var concat = 'rgba( ' + formula * 255 + ', 0, 0, 1.0 )'
         $(element).css('background-color', concat)
+
     }
 
     globalID = requestAnimationFrame(repeatOften)

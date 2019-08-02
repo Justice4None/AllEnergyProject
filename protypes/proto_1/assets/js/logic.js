420a0a7215102f07d2e14df7e8c77768abb48ad2
William Dunn <pikablu112@gmail.com>
2019-08-01 18:13:27 -0700

    Initial SFX complete, will add more if needed

--- protypes/proto_1/assets/js/logic.js
+++ protypes/proto_1/assets/js/logic.js
@@ -96,14 +96,14 @@ var render = {
 }
 
 var audio = {
-    files: ["acolyte-chant-1", "acolyte-chant-2", "acolyte-chant-3", "acolyte-chant-4", "acolyte-chant-5", "acolyte-chant-6", "acolyte-chant-7", "acolyte-chant-8", "acolyte-chant-9", "acolyte-chant-10", "acolyte-chant-11", "acolyte-chant-12", "acolyte-chant-13", "acolyte-chant-long", "acolyte-march-footsteps-1", "arrow-impact-1", "desolving", "invalid-click", "level-up", "magic-spell-1", "monster-sounds-1", "monster-sounds-2", "monster-sounds-3", "monster-sounds-4", "monster-sounds-5", "monster-sounds-6", "monster-sounds-7", "monster-sounds-8", "monster-sounds-9", "monster-sounds-10", "new-map", "succubus-fire-1", "succubus-laugh-1"],
+    files: ["acolyte-chant-1", "acolyte-chant-2", "acolyte-chant-3", "acolyte-chant-4", "acolyte-chant-5", "acolyte-chant-6", "acolyte-chant-7", "acolyte-chant-8", "acolyte-chant-9", "acolyte-chant-10", "acolyte-chant-11", "acolyte-chant-12", "acolyte-chant-13", "acolyte-chant-long", "acolyte-march-footsteps-1", "arrow-impact-1", "bird-calls-high", "bird-calls-low", "desolving", "insect-swarm-1", "invalid-click", "level-up", "magic-spell-1", "monster-sounds-1", "monster-sounds-2", "monster-sounds-3", "monster-sounds-4", "monster-sounds-5", "monster-sounds-6", "monster-sounds-7", "monster-sounds-8", "monster-sounds-9", "monster-sounds-10", "new-map", "succubus-fire-1", "succubus-laugh-1"],
     noiseMakers: [],
     //EXAMPLE MASTERS SOUNDS, NOT REPRESENTATIVE OF FINAL AUDIO
-    masterSounds: ["bird-calls-high", "bird-calls-low"],
+    masterSounds: ["music-track-1", "music-track-2"],
     activeSounds: [],
     load: function () {
         for (i = 0; i < this.masterSounds.length; i++) {
-            masterSounds[masterSounds.length] = new Audio("audio/sfx" + masterSounds[i] + ".mp3")
+            masterSounds[masterSounds.length] = new Audio("audio/music" + masterSounds[i] + ".mp3")
         }
     },
     play: function () {

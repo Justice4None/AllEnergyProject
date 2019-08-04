d56b39bd72b91d13c5d774482c023cd85d5d6a46
Rebeccahdz10 <rebeccahdz10@gmail.com>
2019-08-04 15:18:23 -0700

    towers show

--- protypes/proto_1/js/columns.js
+++ protypes/proto_1/js/columns.js
@@ -5,6 +5,7 @@ var userScore = null;
 var healthScore = null;
 var buildingType = null;
 
+
 //HTML ELEMENTS
 var leftColumn = null;
 var rightColumn = null;
@@ -17,15 +18,52 @@ $(document).ready(function () {
 
     // WILL CHANGE WHEN BUTTONS ARE FINISHED
     $("#submitButt").on('click', function (event) {
-        console.log("stuff is working");
         event.preventDefault();
 
         nav.style.display = "none";
 
+        var h1 = document.createElement('H1');
+
         leftColumn.className = "leftCol";
         rightColumn.className = "rightCol";
 
+        rightColumn.id = "rightColumn"
+
         $('#newDiv').append(leftColumn);
         $('#newDiv').append(rightColumn);
+
+        h1.innerHTML = "Add a Tower!"
+        $('#rightColumn').append(h1);
+
+
+        var tower = $('#rightColumn');
+
+        var towerImages = [
+            { name: 'tower1', image: "../proto_1/gfx/buildings/tower_1.png" },
+            { name: 'tower2', image: "../proto_1/gfx/buildings/tower_2.png" },
+            { name: 'tower3', image: "../proto_1/gfx/buildings/tower_3.png" }
+        ];
+
+        $.each(towerImages, function () {
+
+            tower.append($('<img src="' + this.image + '">').addClass('image'));
+        });
+    });
+
+    //FUNCTION TO ADD TOWER VALUE
+    function swtichTower() {
+
+
+
+    }
+
+    //FUNCTION TO SWITCH TOWER VALUE WHEN CLICKED
+    $(".image").on('click', function () {
+        towerVal = $(this).val();
+
+
+
+
+
     });
 });

b25cd420bcffb2496b50bdf2d718a97a7f1db004
Quibnar <dirk.stockton@gmail.com>
2019-08-05 11:46:35 -0700

    kitty changes

--- protypes/kitty_scripts/js/kitty_scripts.js
+++ protypes/kitty_scripts/js/kitty_scripts.js
@@ -2,12 +2,14 @@
 
 var kitty = {
     transitionSpeed: 1,
-    parallax: function (layersArray, x, y, aperture, influence) {
+    parallax: function (layersArray) {
         for (var i = 0; i < layersArray.length; i++) {
-            $(layersArray[i]).css('left', influence * x * (aperture * i + 1) + "px")
+            var pw = $(layersArray[i]).parent().css('width')
+            $(layersArray[i]).css('left', x / pw * (i + 1) + "px")
             $(layersArray[i]).css('position', "fixed")
-            $(layersArray[i]).css('top', influence * y * (aperture * i + 1) + "px")
+            $(layersArray[i]).css('top', y * (i + 1) + "px")
         }
+        console.log($(layersArray[i]).parent().id)
     },
     parallaxFadeIn: function (layersArray, x, y, aperture, influence) {
         for (var i = 0; i < layersArray.length; i++) {
@@ -18,16 +20,15 @@ var kitty = {
     },
     parallaxOnScroll: function () {
 
-    }
-    fadeIn: function (element, direction) {
+    },
+    fadeIn: function (elem, direction) {
 
     },
-    fadeOut: function (element, direction) {
+    fadeOut: function (elem, direction) {
 
     },
     sparkle: function (elem) {
         var can = $("<canvas></canvas>").css("background-color", "#00ff00")
-        //can.style.backgroundColor = '#00ff00'
         $(elem).append(can)
     },
     sparkleOn: false,
@@ -35,8 +36,13 @@ var kitty = {
         //convert to 3D
         //texture to theme
     },
-    wiggle: function (elem, xMagnitude, yMagnitude) {
-
+    wiggle: function (elem) {
+        $(elem).addClass('horizTranslate');
+    },
+    stopWiggle: function (elem) {
+        var computedStyle = $(elem).css('margin-left');
+        $(elem).removeClass('horizTranslate');
+        $(elem).css('margin-left', computedStyle);
     },
     shake: function (elem, xMagnitude, yMagnitude) {
 
